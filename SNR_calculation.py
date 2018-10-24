c3b027c21675496664abcc372bc313349b82a2ee
Christos Georgiou <georgiou@strw.leidenuniv.nl>
2018-10-24 14:14:56 +0200

    Testing developing.

--- SNR_calculation.py
+++ SNR_calculation.py
@@ -2,9 +2,27 @@ import numpy as np
 from astropy.io import fits
 
 
-def SNR(image, wf):
+def SNR(imagedata, wf, noise_var):
+	"""
+
+	:param imagedata: 2-D array containing the image data.
+	:param wf: Weight-function function (see "weight_function()).
+	:param noise_var: Variance of the noise in the image. Assumed to be constant.
+	:return: Signal-to-Noise ratio of your image, weighted with the weight function employed.
+	"""
 	# TODO: What if the image is too large? Have a check
-	imagedata = fits.getdata(image)
+	#imagedata = fits.getdata(image, 1) # FIXME: what should the number be here? Put a try/except statement or something?
+	x = np.arange(-imagedata.shape[0]/2, imagedata.shape[0]/2, step=1, dtype=np.int)
+	y = np.arange(-imagedata.shape[1] / 2, imagedata.shape[1] / 2, step=1, dtype=np.int)
+	x, y = np.meshgrid(x, y)
+	wf_= np.reshape(map(wf, x.flatten(), y.flatten()), (len(x[0]), len(y[0])))
+	print np.sum(wf_), np.trapz(np.trapz(wf_, x[0,:]), y[:,0])
+	nom = np.sum(imagedata*wf_)**2.#np.trapz(np.trapz(imagedata*wf_, x[0,:]), y[:,0])**2.
+	denom = np.sum(noise_var*wf_**2.)#np.trapz(np.trapz(noise_var*wf_**2., x[0,:]), y[:,0])
+	print np.sqrt(nom), np.sqrt(denom)
+	# TODO: Cross-check with DEIMOS. Questions: Do I get the flux right? np.sum or 2*np.trapz (which is faster)? Is the nom/denom correct?
+	# TODO: SNR increases with wf scale, flux decreases (greatly!!). Very large wf scale does not return image sum.
+	return np.sqrt(nom/denom)
 
 
 def weight_function(scale, e1=0.0, e2=0.0, x_cen=0.0, y_cen=0.0):
@@ -13,21 +31,23 @@ def weight_function(scale, e1=0.0, e2=0.0, x_cen=0.0, y_cen=0.0):
 	:param scale: Scale factor of the gaussian. Units should be the same as the coordinate system's units.
 	:param e1: ellipticity component along the x-axis
 	:param e2: ellipticity component along the y-axis
+	:param x_cen: Shift of the weight function in the direction of the x-axis.
+	:param y_cen: ...of the y-axis.
 	:return: a python lambda function that can be called at any (x,y) point.
 
-	Usage:
-	r = weight_function(1., 0.5, 0.6)
+	# Usage:
+	wf = weight_function(1., 0.5, 0.6)
 	x = np.linspace(-1, 1, 300) # Desired values in x-axis where to evaluate the weight function
 	y = np.linspace(-1, 1, 300) # ...in y-axis
 	x, y = np.meshgrid(x,y) # Produces two (300, 300) arrays with the same column (or row) for each row (or column).
-	wf = r(0.2, 0.5) # returns the value of the weight function at (x,y)=(0.2,0.5)
-	wf = map(r, x.flatten(), y.flatten()) # Returns values of the weight function at all 300x300 points in a single 1-D array
-	wf = np.reshape(wf, (len(x[0]), len(y[0]))) # Reshapes the 1-D array into (300,300)
+	wf_ = wf(0.2, 0.5) # returns the value of the weight function at (x,y)=(0.2,0.5)
+	wf_ = map(wf, x.flatten(), y.flatten()) # Returns values of the weight function at all 300x300 points in a single 1-D array
+	wf_ = np.reshape(wf_, (len(x[0]), len(y[0]))) # Reshapes the 1-D array into (300,300)
 	import matplotlib.pyplot as plt
 	plt.pcolormesh(wf)
 	plt.colorbar()
 	plt.show()
-	flux = np.trapz(np.trapz(wf, x[0,:]), y[:,0]) # This is equal to 1.
+	flux = np.trapz(np.trapz(wf_, x[0,:]), y[:,0]) # This is equal to 1.
 	"""
 	assert e1**2.+e2**2. < 1., "Ellipticity larger than 1."
 	C = np.matrix([[1-e1, -e2], [-e2, 1+e1]])
@@ -61,3 +81,13 @@ def create_poststamp(image, x_cen, y_cen, x_size, y_size):
 	# FIXME: THE THING BELOW NOW WORKS ONLY FOR FLOAT CENTROIDS NOT ARRAYS
 	image_ps = imagedata[y_cen_int - y_size/2:y_cen_int + y_size/2, x_cen_int - x_size/2:x_cen_int + x_size/2]
 	return image_ps
+
+# test
+image = 'testimage.fits'
+x_cen, y_cen = 150, 150
+x_lim, ylim = 100, 100
+ps = create_poststamp(image, x_cen, y_cen, x_lim, ylim)
+wf = weight_function(10)
+print SNR(ps, wf, 0.01)
+# Flux should be ~25.
+# TODO: Compare with image_moments.py
--- tests.py
+++ tests.py
@@ -1,18 +1,18 @@
 from SNR_calculation import *
 
 # Test the weight function function:
-r = weight_function(0.1, 0.0, 0.0)
-x = np.linspace(-1, 1, 300)
-y = np.linspace(-1, 1, 300)
+r = weight_function(10, 0.0, 0.0)
+x = np.arange(-100, 100, step=1)
+y = np.arange(-100, 100, step=1)
 x, y = np.meshgrid(x,y)
 # How much time does the reshaping take?
-import time
-t1 = time.time()
+#import time
+#t1 = time.time()
 wf = np.reshape(map(r, x.flatten(), y.flatten()), (len(x[0]), len(y[0])))
-t2=time.time()
-wf_temp = map(r, x.flatten(), y.flatten()), (len(x[0]), len(y[0]))
-t3=time.time()
-print 'just func: %s, func and reshape: %s' %(t3-t2, t2-t1)
+#t2=time.time()
+#wf_temp = map(r, x.flatten(), y.flatten()), (len(x[0]), len(y[0]))
+#t3=time.time()
+#print 'just func: %s, func and reshape: %s' %(t3-t2, t2-t1)
 
 import matplotlib.pyplot as plt
 plt.pcolormesh(wf)
@@ -26,4 +26,24 @@ x_lim, ylim = 50, 50
 ps = create_poststamp(image, x_cen, y_cen, x_lim, ylim)
 import matplotlib.pyplot as plt
 plt.pcolormesh(ps)
+plt.show()
+
+# test the weight function centroid change
+image = '/data/KIDS450_129.0_-0.5_r_sci.fits'
+x_cen, y_cen = 7754.01, 2662.7
+x_lim, ylim = 50, 50
+ps = create_poststamp(image, x_cen, y_cen, x_lim, ylim)
+wf = weight_function(10000, 0.0, 0.0, x_cen=6, y_cen=-3)
+print SNR(ps, wf, 2e-12)
+
+
+x = np.arange(-ps.shape[0]/2, ps.shape[0]/2, step=1) # Desired values in x-axis where to evaluate the weight function
+y = np.arange(-ps.shape[1]/2, ps.shape[1]/2, step=1) # Desired values in y-axis where to evaluate the weight function
+x, y = np.meshgrid(x,y) # Produces two (300, 300) arrays with the same column (or row) for each row (or column).
+wf_ = map(wf, x.flatten(), y.flatten()) # Returns values of the weight function at all 300x300 points in a single 1-D array
+wf_ = np.reshape(wf_, (len(x[0]), len(y[0])))
+import matplotlib.pyplot as plt
+plt.pcolormesh(ps)
+plt.colorbar()
+plt.contour(wf_)
 plt.show()
